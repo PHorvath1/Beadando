d186fa289cc03aac1955ed00f1f96ca3330e3d05
Peter <hpeti291@gmail.com>
2020-05-01 12:37:09 +0200

    Added login and register functions.

--- protected/nav.php
+++ protected/nav.php
@@ -1,1 +1,7 @@
-Nav
+<a href="index.php">Home</a>
+<?php if(!IsUserLoggedIn()) : ?>
+    <a id="Login" href="index.php?P=login">Login</a>
+    <a id="Register" href="index.php?P=register">Register</a>
+<?php else: ?>
+    <a id="Logout" href="index.php?P=logout">Logout</a>
+<?php endif; ?>
